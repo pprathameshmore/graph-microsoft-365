1f8044b2d138f8b1f9e0a41e22e6f4d16dbc0306
Michael Knoedel <mknoedel@gmail.com>
2021-03-08 17:15:19 -0600

    ingest device compliance policies and add function keys to policy and config entities

--- src/steps/intune/steps/compliancePoliciesAndFindings/converters.ts
+++ src/steps/intune/steps/compliancePoliciesAndFindings/converters.ts
@@ -5,47 +5,43 @@ import {
   parseTimePropertyValue,
 } from '@jupiterone/integration-sdk-core';
 import {
-  DeviceConfiguration,
+  DeviceCompliancePolicy,
   DeviceConfigurationDeviceStatus,
 } from '@microsoft/microsoft-graph-types-beta';
 import { entities } from '../../constants';
+import { last } from 'lodash';
 import {
   calculateSeverity,
   calculateNumericSeverity,
   findingIsOpen,
 } from '../../utils';
-import { last } from 'lodash';
 
 /**
- * There are several different types of device configurations based on your OS and what you are configuring.
+ * There are several different types of compliance policies based on your OS and what you are configuring.
  * So many that it does not make sense to ingest each individually until that granularity is desired
- * or necessary. Because of that, this method ingests all device configurations the same way, and if
+ * or necessary. Because of that, this method ingests all compliance policies the same way, and if
  * more granularity is desired, individual configuration settings can be viewed in the raw data.
  *
- * https://docs.microsoft.com/en-us/graph/api/resources/intune-shared-deviceconfiguration?view=graph-rest-beta
+ * https://docs.microsoft.com/en-us/graph/api/resources/intune-shared-devicecompliancepolicy?view=graph-rest-beta
  */
-export function createDeviceConfigurationEntity(
-  deviceConfiguration: DeviceConfiguration,
+export function createCompliancePolicyEntity(
+  compliancePolicy: DeviceCompliancePolicy,
 ): Entity {
   return createIntegrationEntity({
     entityData: {
-      source: deviceConfiguration,
+      source: compliancePolicy,
       assign: {
-        _class: entities.DEVICE_CONFIGURATION._class,
-        _type: entities.DEVICE_CONFIGURATION._type,
-        function: 'endpoint-configuration', // needed to match up with existing integrations data structures.
-        id: deviceConfiguration.id,
-        name: deviceConfiguration.displayName,
+        _class: entities.COMPLIANCE_POLICY._class,
+        _type: entities.COMPLIANCE_POLICY._type,
+        function: 'endpoint-compliance', // needed to match up with existing integrations data structures.
+        id: compliancePolicy.id,
+        name: compliancePolicy.displayName,
         description:
-          deviceConfiguration.description || deviceConfiguration.displayName,
-        createdOn: parseTimePropertyValue(deviceConfiguration.createdDateTime),
-        displayName: deviceConfiguration.displayName,
-        version: deviceConfiguration.version,
-        /**
-         * Possible values (There are many of them): https://docs.microsoft.com/en-us/graph/api/resources/intune-device-cfg-conceptual?view=graph-rest-beta
-         * examples: iosCustomConfiguration, windows10GeneralConfiguration, iosWiFiConfiguration
-         */
-        configurationType: last(deviceConfiguration['@odata.type']?.split('.')),
+          compliancePolicy.description || compliancePolicy.displayName,
+        createdOn: parseTimePropertyValue(compliancePolicy.createdDateTime),
+        displayName: compliancePolicy.displayName,
+        version: compliancePolicy.version,
+        policyType: last(compliancePolicy['@odata.type']?.split('.')), // Possible values: 'iosCompliancePolicy'
       },
     },
   });
@@ -54,7 +50,7 @@ export function createDeviceConfigurationEntity(
 // https://docs.microsoft.com/en-us/graph/api/resources/intune-deviceconfig-deviceconfigurationdevicestatus?view=graph-rest-beta
 export function createNoncomplianceFindingEntity(
   deviceStatus: DeviceConfigurationDeviceStatus,
-  deviceConfigurationEntity: Entity,
+  compliancePolicyEntity: Entity,
   logger: IntegrationLogger,
 ): Entity {
   return createIntegrationEntity({
@@ -64,9 +60,9 @@ export function createNoncomplianceFindingEntity(
         _class: entities.NONCOMPLIANCE_FINDING._class,
         _type: entities.NONCOMPLIANCE_FINDING._type,
         id: deviceStatus.id,
-        name: 'Latest finding from ' + deviceConfigurationEntity.displayName,
+        name: 'Latest finding from ' + compliancePolicyEntity.displayName,
         category: 'endpoint',
-        assessment: deviceConfigurationEntity.displayName,
+        assessment: compliancePolicyEntity.displayName,
         status: deviceStatus.status, // Possible values are: unknown, notApplicable, compliant, remediated, nonCompliant, error, conflict, notAssigned.
         severity: calculateSeverity(deviceStatus.status, logger),
         numericSeverity: calculateNumericSeverity(deviceStatus.status, logger),
